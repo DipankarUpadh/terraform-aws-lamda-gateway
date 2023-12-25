5f9cc0c5bc6eda3ef3fe7c5783e139b5c8e7e2b7
DipankarUpadh <52188967+DipankarUpadh@users.noreply.github.com>
2023-12-25 20:27:06 +0530

    add Lambda

--- main.tf
+++ main.tf
@@ -40,3 +40,46 @@ resource "aws_s3_object" "lambda_java" {
 
   etag = filemd5("AwsLamda.jar")
 }
+
+
+resource "aws_lambda_function" "lambda_java" {
+  function_name = "LambdaJava"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_java.key
+
+  runtime = "Java 11"
+  handler = "Basic.handle"
+
+  source_code_hash = AwsLamda.jar.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "lambda_java" {
+  name = "/aws/lambda/${aws_lambda_function.lambda_java.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
