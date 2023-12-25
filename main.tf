a3cf2f11a6dd364769c653ebad758d789fa568f1
DipankarUpadh <52188967+DipankarUpadh@users.noreply.github.com>
2023-12-25 20:48:08 +0530

    add Lambda

--- main.tf
+++ main.tf
@@ -41,19 +41,19 @@ resource "aws_s3_object" "lambda_java" {
   etag = filemd5("AwsLamda.jar")
 }
 
+locals {
+    jar_filename = "AwsLamda.jar"
+}
 
 resource "aws_lambda_function" "lambda_java" {
   function_name = "LambdaJava"
-
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_java.key
-
+  role = aws_iam_role.lambda_exec.arn
   runtime = "Java 11"
   handler = "Basic.handle"
-
-  source_code_hash = base64sha256(filebase64(AwsLamda.jar))
-
-  role = aws_iam_role.lambda_exec.arn
+  source_code_hash = base64sha256(filebase64(locals.jar_filename))
+  
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_java.key
 }
 
 resource "aws_cloudwatch_log_group" "lambda_java" {
