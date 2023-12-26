53385f1e6662a27cd9d332f17ad1dd54293c5036
DipankarUpadh <52188967+DipankarUpadh@users.noreply.github.com>
2023-12-26 23:38:47 +0530

    add API gateway

--- main-api.tf
+++ main-api.tf
@@ -1,12 +1,12 @@
-resource "aws_apigatewayv2_api" "lambda" {
-  name          = "serverless_lambda_gw"
+resource "aws_apigatewayv2_api" "gateway" {
+  name          = "lambda_gw"
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_apigatewayv2_stage" "gateway" {
+  api_id = aws_apigatewayv2_api.gateway.id
 
-  name        = "serverless_lambda_stage"
+  name        = "gateway_stage"
   auto_deploy = true
 
   access_log_settings {
@@ -28,23 +28,23 @@ resource "aws_apigatewayv2_stage" "lambda" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "hello_world" {
-  api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id = aws_apigatewayv2_api.gateway.id
 
-  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+  integration_uri    = aws_lambda_function.lambda_java.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
 
-resource "aws_apigatewayv2_route" "hello_world" {
-  api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_apigatewayv2_route" "route" {
+  api_id = aws_apigatewayv2_api.gateway.id
 
   route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
 resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+  name = "/aws/api_gw/${aws_apigatewayv2_api.gateway.name}"
 
   retention_in_days = 30
 }
@@ -52,7 +52,7 @@ resource "aws_cloudwatch_log_group" "api_gw" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
+  function_name = aws_lambda_function.lambda_java.t_lambda_java
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
--- variables.tf
+++ variables.tf
@@ -6,3 +6,13 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "aws_access_key" {
+  # set aws access key
+  default = ""
+}
+
+variable "aws_secret_key" {
+  # set aws secret key
+  default = ""
+}
