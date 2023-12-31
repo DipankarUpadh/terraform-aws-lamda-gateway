ef5bd71533e1c170610c95cb21877f290670ce69
DipankarUpadh <52188967+DipankarUpadh@users.noreply.github.com>
2023-12-31 16:37:05 +0530

    Configure config

--- role.tf
+++ role.tf
@@ -14,6 +14,6 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "t_iam_role" {
-  name = "AwsServiceRoleForLambdaWithS3Permission"
+  name = "AwsLambdaRoleWithS3Permission"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
