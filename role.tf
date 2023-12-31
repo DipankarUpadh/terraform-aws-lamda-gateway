7a3afeff3507fc867cc77c154382a65c0ce80139
DipankarUpadh <52188967+DipankarUpadh@users.noreply.github.com>
2023-12-31 12:56:50 +0530

    Configure config

--- role.tf
+++ role.tf
@@ -14,6 +14,6 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "t_iam_role" {
-  name = "s3_log_lambda_user"
+  name = resource.aws_iam_user.name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
